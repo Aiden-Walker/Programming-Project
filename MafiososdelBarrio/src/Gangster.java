2cc9a946695537afaf2e33626d841fdefc8b28ab
Aiden-Walker <thebusinessgman@gmail.com>
2025-02-07 18:34:32 +0100

    Addind new stuff to the repository

---
+++ MafiososdelBarrio/src/Gangster.java
@@ -0,0 +1,15 @@
+public class Gangster {
+    int workday;
+    String gangsterName;
+    int work_days;
+    public Gangster(String gangsterName){
+        this.gangsterName=gangsterName;
+    }
+
+    public void setWorkday(int workday) {
+        this.workday = workday;
+    }
+    public int getWorkday(){
+        return this.workday;
+    }
+}
---
+++ MafiososdelBarrio/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.HashSet;
+import Gangster;
+public class Main
+{
+    public static void main(String[] args) {
+        HashSet<Gangster>Gangsters = new HashSet<Gangster>(Gangsters);
+    }
+}
